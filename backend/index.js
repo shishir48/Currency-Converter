4c997d205a3d1a5322b993bd0829f26393e2b494
shishir singh <shishir.singh@eisenvault.com>
2024-01-05 14:56:24 +0530

    trying to deploy backend to vercel

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
 require("dotenv").config()
 const express = require("express")
-const app = express()
 const cors = require("cors")
-
-const app = require("./app")
+const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use("/api", require("./routes/routes"))
+app.use("/api", require("./src/routes/routes"))
 
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => console.info(`crypto service running on port ${PORT}`))
--- backend/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-
-app.use(cors())
-app.use(express.json())
-app.use("/api", require("./routes/routes"))
-
-module.exports = app
