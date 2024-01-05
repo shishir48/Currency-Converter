9746da84424ad4efc067e99785a1d10cec5b7236
shishir singh <shishir.singh@eisenvault.com>
2024-01-05 14:46:36 +0530

    changes for vercel deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
 require("dotenv").config()
+const express = require("express")
+const app = express()
+const cors = require("cors")
 
 const app = require("./app")
 
+app.use(cors())
+app.use(express.json())
+app.use("/api", require("./routes/routes"))
+
 const PORT = process.env.PORT || 3002
 app.listen(PORT, () => console.info(`crypto service running on port ${PORT}`))
+
+module.exports = app
